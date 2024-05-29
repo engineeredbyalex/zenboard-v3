ccb3acdbfbaf9ce3908ce60552657d770f693044
engineeredbyalex <dev.lazarescu.alexandru@gmail.com>
2024-05-29 20:06:52 +0300

    29.01 rev-1

--- pages/products.js
+++ pages/products.js
@@ -1,15 +1,15 @@
 // importing layout
 import Layout from "@/components/Layout";
-// importing link
-import Link from "next/link";
 // importing useEffect and useState
 import { useEffect, useState } from "react";
 // importing axios
 import axios from "axios";
+import Link from "next/link";
 
 export default function Products() {
   const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+
   useEffect(() => {
     setIsLoading(true);
     axios.get('/api/products/products').then(response => {
@@ -17,6 +17,16 @@ export default function Products() {
       setIsLoading(false);
     });
   }, []);
+
+  const deleteProduct = async (id) => {
+    try {
+      await axios.delete(`/api/products/products?id=${id}`);
+      setProducts(products.filter(product => product._id !== id));
+    } catch (error) {
+      console.error('Failed to delete the product:', error);
+    }
+  };
+
   return (
     <Layout>
       <div className="text-center">
@@ -24,10 +34,10 @@ export default function Products() {
           Products
         </h3>
       </div>
-      <div className=" flex items-center justify-center gap-5 flex-col lg:flex-row text-center lg:text-left">
-        <Link className="btn-primary uppercase font-bold w-full lg:w-1/3 " href={'/products/new'}>Add a new product</Link>
-        <Link className="btn-primary uppercase font-bold w-full lg:w-1/3 " href={'/products/view'}>See all products</Link>
-        <Link className="btn-primary uppercase font-bold w-full lg:w-1/3 " href={'/products/settings'}>Product settings</Link>
+      <div className="flex items-center justify-center gap-5 flex-col lg:flex-row text-center lg:text-left">
+        <Link className="btn-primary uppercase font-bold w-full lg:w-1/3" href={'/products/new'}>Add a new product</Link>
+        <Link className="btn-primary uppercase font-bold w-full lg:w-1/3" href={'/products/view'}>See all products</Link>
+        <Link className="btn-primary uppercase font-bold w-full lg:w-1/3" href={'/products/settings'}>Product settings</Link>
       </div>
       <table className="basic mt-2">
         <thead>
@@ -41,7 +51,9 @@ export default function Products() {
         </thead>
         <tbody>
           {isLoading && (
-         null
+            <tr>
+              <td>Loading...</td>
+            </tr>
           )}
           {products.map(product => (
             <tr className="flex flex-col" key={product._id}>
@@ -50,19 +62,19 @@ export default function Products() {
                   {product.title}
                 </h5>
               </td>
-              <td className=" flex items-center justify-center gap-3 lg:gap-5 flex-col lg:flex-row text-center lg:text-left">
+              <td className="flex items-center justify-center gap-3 lg:gap-5 flex-col lg:flex-row text-center lg:text-left">
                 <Link className="btn-primary w-full" href={'/products/edit/' + product._id}>
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                   </svg>
                   Edit
                 </Link>
-                <Link className="btn-red w-full" href={'/products/delete/' + product._id}>
+                <button className="btn-red w-full" onClick={() => deleteProduct(product._id)}>
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                   </svg>
                   Delete
-                </Link>
+                </button>
               </td>
             </tr>
           ))}
